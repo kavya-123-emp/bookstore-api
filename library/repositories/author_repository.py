589dbe0ad5ef5b4c6787552bbe5bc4c012bccc71
kavya-123-emp <kavyaramachandran05@gmail.com>
2025-07-30 18:46:33 +0530

    Update author_repository.py

--- library/repositories/author_repository.py
+++ library/repositories/author_repository.py
@@ -1,1 +1,16 @@
 # Author data access logic here
+# Author data access logic here
+from library.models import Author
+
+class AuthorRepository:
+    @staticmethod
+    def get_all():
+        return Author.objects.all()
+
+    @staticmethod
+    def get_by_id(author_id):
+        return Author.objects.filter(id=author_id).first()
+
+    @staticmethod
+    def create(**kwargs):
+        return Author.objects.create(**kwargs)
