3bb978cae2e2ad7c5fd86e347f2518b5f789ec68
kavya-123-emp <kavyaramachandran05@gmail.com>
2025-07-30 18:43:45 +0530

    Update author_service.py

--- library/services/author_service.py
+++ library/services/author_service.py
@@ -1,1 +1,27 @@
 # Business logic for author here
+# Business logic for author here
+from library.models import Author
+
+class AuthorService:
+    @staticmethod
+    def get_all_authors():
+        return Author.objects.all()
+
+    @staticmethod
+    def get_author_by_id(author_id):
+        return Author.objects.filter(id=author_id).first()
+
+    @staticmethod
+    def create_author(data):
+        return Author.objects.create(**data)
+
+    @staticmethod
+    def update_author(author, data):
+        for key, value in data.items():
+            setattr(author, key, value)
+        author.save()
+        return author
+
+    @staticmethod
+    def delete_author(author):
+        author.delete()
