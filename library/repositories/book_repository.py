26b1d3f3785d51ab9cef0a251ac8d227d0efbbce
kavya-123-emp <kavyaramachandran05@gmail.com>
2025-07-30 18:47:08 +0530

    Update book_repository.py

--- library/repositories/book_repository.py
+++ library/repositories/book_repository.py
@@ -1,1 +1,16 @@
+
 # Book data access logic here
+from library.models import Book
+
+class BookRepository:
+    @staticmethod
+    def get_all():
+        return Book.objects.select_related("author").all()
+
+    @staticmethod
+    def get_by_id(book_id):
+        return Book.objects.filter(id=book_id).first()
+
+    @staticmethod
+    def create(**kwargs):
+        return Book.objects.create(**kwargs)
