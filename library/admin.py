8e27be8e169823dfae1bb09ef75a2dee582e83fd
kavya-123-emp <kavyaramachandran05@gmail.com>
2025-07-30 18:36:36 +0530

    Update admin.py

--- library/admin.py
+++ library/admin.py
@@ -1,1 +1,15 @@
+
 # Admin model registration
+from django.contrib import admin
+from .models import Author, Book
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email')
+    search_fields = ('name',)
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'author', 'published_date')
+    search_fields = ('title',)
+    list_filter = ('author',)
