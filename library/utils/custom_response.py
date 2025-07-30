00d3f76b6715d13841ba35a22c5f5b40e288d52e
kavya-123-emp <kavyaramachandran05@gmail.com>
2025-07-30 18:40:51 +0530

    Update custom_response.py

--- library/utils/custom_response.py
+++ library/utils/custom_response.py
@@ -1,1 +1,17 @@
+
 # Custom response formatting functions
+from rest_framework.response import Response
+
+def success_response(data=None, message="Success", status_code=200):
+    return Response({
+        "status": "success",
+        "message": message,
+        "data": data
+    }, status=status_code)
+
+def error_response(errors=None, message="Error", status_code=400):
+    return Response({
+        "status": "error",
+        "message": message,
+        "errors": errors
+    }, status=status_code)
