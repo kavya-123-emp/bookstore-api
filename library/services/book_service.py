72c6570b0dee53d06b5fef496e704d528433979c
kavya-123-emp <kavyaramachandran05@gmail.com>
2025-07-30 18:43:17 +0530

    Update book_service.py

--- library/services/book_service.py
+++ library/services/book_service.py
@@ -1,1 +1,27 @@
 # Business logic for book here
+# Business logic for book here
+from library.models import Book
+
+class BookService:
+    @staticmethod
+    def get_all_books():
+        return Book.objects.select_related("author").all()
+
+    @staticmethod
+    def get_book_by_id(book_id):
+        return Book.objects.filter(id=book_id).first()
+
+    @staticmethod
+    def create_book(data):
+        return Book.objects.create(**data)
+
+    @staticmethod
+    def update_book(book, data):
+        for key, value in data.items():
+            setattr(book, key, value)
+        book.save()
+        return book
+
+    @staticmethod
+    def delete_book(book):
+        book.delete()
